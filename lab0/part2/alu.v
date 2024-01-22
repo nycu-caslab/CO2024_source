1b57c3233a70d99e886867f16a26b55d25bf727e
kaijhsu <hikper@gmail.com>
2024-01-22 12:48:36 +0800

    Add description, Makefile

--- lab0/part2/alu.v
+++ lab0/part2/alu.v
@@ -1,9 +1,16 @@
-// module alu(a, b, s, y);
-//     input signed [3:0] a;  
-//     input signed [3:0] b;  
-//     input [2:0] s; 
-//     output reg signed [3:0] y;  
-module alu(input signed [3:0] a, input signed [3:0] b, input [2:0] s, output reg signed [3:0] y); 
+/*
+*  Description
+*    - alu has two input operand a and b.
+*    - alu executes different operation over input a and b based on input s
+*    - alu outputs final result to y
+*/
+
+module alu(
+    input signed [3:0] a,
+    input signed [3:0] b,
+    input [2:0] s,
+    output reg signed [3:0] y
+); 
     
     // TODO: implement your 4bits ALU design here and using your own fulladder module in this module
     // For testbench verifying, do not modify input and output pin
